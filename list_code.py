a1b56700a29fd273880baf4dc5a8d78de55acb59
nayanprakash06 <124758491+nayanprakash06@users.noreply.github.com>
2025-03-19 23:06:54 +0530

    Add files via upload

---
+++ list_code.py
@@ -0,0 +1,17 @@
+mybag = ['laptop',1,'books',4,'keys','ID','bottle']
+
+mybag.append('lactose_Free_milk')
+
+print(len(mybag)) #total number of elements in the list, mybag
+print(mybag[7])
+print(mybag)
+mybag.append(11)
+print(mybag)
+
+print(mybag[-2])
+
+mybag[3] = 5
+print(mybag)
+mybag[3] = 'cat'
+print(mybag)
+
